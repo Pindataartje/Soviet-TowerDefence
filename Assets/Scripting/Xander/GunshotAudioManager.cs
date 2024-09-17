f1ecfa657f6ff6b87d7783c00cac2941507c40a9
Pindataartje <xbruintjes@gmail.com>
2024-09-17 13:00:09 +0200

    MainMenuGunshots

---
+++ Assets/Scripting/Xander/GunshotAudioManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class GunshotAudioManager : MonoBehaviour
+{
+    public ParticleSystem gunshotParticles;
+    public AudioSource gunshotAudioSource;
+    public AudioClip gunshotClip;
+
+    private int previousParticleCount;
+
+    void Start()
+    {
+        // Initialize the previousParticleCount
+        previousParticleCount = 0;
+    }
+
+    void Update()
+    {
+        int currentParticleCount = gunshotParticles.particleCount;
+
+        // Check if new particles have been emitted
+        if (currentParticleCount > previousParticleCount)
+        {
+            PlayGunshotSound();
+        }
+
+        // Update the previousParticleCount for the next frame
+        previousParticleCount = currentParticleCount;
+    }
+
+    private void PlayGunshotSound()
+    {
+        gunshotAudioSource.PlayOneShot(gunshotClip);
+    }
+}
